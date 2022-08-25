1f6b6c367d386db75c4d5d30ae9781df8e1344b3
Akhat Kulzhanov <kulzanovahat@gmail.com>
2022-08-26 01:10:36 +0600

    comments

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
-'''Initializing application and DB'''
+'''Initializing application and DB (SQLite)'''
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///registration.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
